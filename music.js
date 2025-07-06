4ed3a721b3743ad95710322757fc9c5321fc8bcf
Davanico <davaniko40@gmail.com>
2025-07-07 02:04:22 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleBtn = document.getElementById('togglePlaylist');
-  const closeBtn = document.getElementById('closePlaylist');
-  const musicPanel = document.getElementById('musicPanel');
+const openBtn = document.querySelector('.open-btn');
+const musicSlide = document.querySelector('.music-slide');
+const body = document.body;
 
-  if (toggleBtn && closeBtn && musicPanel) {
-    toggleBtn.addEventListener('click', () => {
-      musicPanel.classList.add('active');
-    });
-    closeBtn.addEventListener('click', () => {
-      musicPanel.classList.remove('active');
-    });
+openBtn.addEventListener('click', () => {
+  musicSlide.classList.toggle('active');
+
+  // Toggle scroll website
+  if (musicSlide.classList.contains('active')) {
+    body.classList.add('no-scroll');
+  } else {
+    body.classList.remove('no-scroll');
   }
 });
