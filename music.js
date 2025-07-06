d6b02417b676d7ce99c2c6678d531e6ea15f874e
Davanico <davaniko40@gmail.com>
2025-07-07 02:12:16 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,16 +1,11 @@
 const toggleBtn = document.getElementById('togglePlaylist');
 const closeBtn = document.getElementById('closePlaylist');
 const musicPanel = document.getElementById('musicPanel');
-const body = document.body;
 
-// Toggle panel
 toggleBtn.addEventListener('click', () => {
   musicPanel.classList.add('active');
-  body.classList.add('no-scroll');
 });
 
-// Tutup panel
 closeBtn.addEventListener('click', () => {
   musicPanel.classList.remove('active');
-  body.classList.remove('no-scroll');
 });
