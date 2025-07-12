d6c9f171534efd6de4cffc797b66b5bc8c5870dc
Davanico <davaniko40@gmail.com>
2025-07-12 17:58:29 +0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -13,7 +13,7 @@ contactForm.addEventListener("submit", function (e) {
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
-  fetch("https://kirimpesan.vercel.app/api/send-message", {
+fetch("https://dvw12.vercel.app/api/send-message", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
